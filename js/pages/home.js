cca662bf06147817bb245f3b420ab56a6b7fef17
ManJurkus <129725669+ManJurkus@users.noreply.github.com>
2023-06-05 13:42:55 +0300

    experience functionality done

--- js/pages/home.js
+++ js/pages/home.js
@@ -43,6 +43,65 @@ socialsDOM.innerHTML = HTML;
 /*SERVICES end */
 
 /*OPTIMIZE start */
+// const pliusDOM = document.getElementById('plius');
+const pliusDOM = document.querySelectorAll('#plius');
+const minusDOM = document.querySelectorAll('#minus');
+const boxDOM = document.querySelectorAll('#experience-box');
+const pDOM = document.querySelectorAll('#hidden-p');
+
+console.log(pliusDOM);
+console.log(minusDOM);
+console.log(boxDOM);
+console.log(pDOM);
+
+function reveal1() {
+    minusDOM[0].classList.remove('not-visable');
+    pliusDOM[0].classList.add('not-visable');
+    pDOM[0].classList.remove('not-visable');
+
+    minusDOM[1].classList.add('not-visable');
+    pliusDOM[1].classList.remove('not-visable');
+    pDOM[1].classList.add('not-visable');
+
+    minusDOM[2].classList.add('not-visable');
+    pliusDOM[2].classList.remove('not-visable');
+    pDOM[2].classList.add('not-visable');
+}
+
+boxDOM[0].addEventListener('click', reveal1);
+
+function reveal2() {
+    minusDOM[1].classList.remove('not-visable');
+    pliusDOM[1].classList.add('not-visable');
+    pDOM[1].classList.remove('not-visable');
+
+    minusDOM[0].classList.add('not-visable');
+    pliusDOM[0].classList.remove('not-visable');
+    pDOM[0].classList.add('not-visable');
+
+    minusDOM[2].classList.add('not-visable');
+    pliusDOM[2].classList.remove('not-visable');
+    pDOM[2].classList.add('not-visable');
+}
+
+boxDOM[1].addEventListener('click', reveal2);
+
+function reveal3() {
+    minusDOM[2].classList.remove('not-visable');
+    pliusDOM[2].classList.add('not-visable');
+    pDOM[2].classList.remove('not-visable');
+
+    minusDOM[0].classList.add('not-visable');
+    pliusDOM[0].classList.remove('not-visable');
+    pDOM[0].classList.add('not-visable');
+
+    minusDOM[1].classList.add('not-visable');
+    pliusDOM[1].classList.remove('not-visable');
+    pDOM[1].classList.add('not-visable');
+}
+
+boxDOM[2].addEventListener('click', reveal3);
+
 /*OPTIMIZE end */
 
 /*MARKETING AGENCY start*/
